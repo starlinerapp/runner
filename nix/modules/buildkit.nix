61c6d7bfefcd5b3bdc000ded23602183ae717b3f
Leon Liang <hello@leonliang.lu>
2026-06-06 12:41:59 +0200

    add runc

--- nix/modules/buildkit.nix
+++ nix/modules/buildkit.nix
@@ -12,35 +12,55 @@
 
   environment.systemPackages = with pkgs; [
     buildkit
+    runc
     git
     curl
     cacert
   ];
 
+  environment.etc."buildkit/buildkitd.toml".text = ''
+    [worker.oci]
+      enabled = true
+      binary = "${pkgs.runc}/bin/runc"
+      rootless = false
+
+    [worker.containerd]
+      enabled = false
+  '';
+
   systemd.services.buildkitd = {
     description = "BuildKit Daemon";
 
-    wantedBy = ["multi-user.target"];
+    wantedBy = [ "multi-user.target" ];
 
-    after = ["network-online.target"];
-    wants = ["network-online.target"];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    path = with pkgs; [
+      buildkit
+      runc
+      iptables
+    ];
 
     serviceConfig = {
-        Type = "simple";
+      Type = "simple";
 
-        ExecStart = ''
-          ${pkgs.buildkit}/bin/buildkitd \
-            --addr unix:///run/buildkit/buildkitd.sock \
-            --addr tcp://0.0.0.0:1234
-        '';
+      ExecStart = ''
+        ${pkgs.buildkit}/bin/buildkitd \
+          --config /etc/buildkit/buildkitd.toml \
+          --addr unix:///run/buildkit/buildkitd.sock \
+          --addr tcp://0.0.0.0:1234
+      '';
 
-        Restart = "always";
-        RestartSec = 5;
+      Restart = "always";
+      RestartSec = 5;
 
-        LimitNOFILE = 1048576;
+      LimitNOFILE = 1048576;
 
-        StateDirectory = "buildkit";
-        RuntimeDirectory = "buildkit";
+      StateDirectory = "buildkit";
+      RuntimeDirectory = "buildkit";
+
+      Delegate = true;
     };
   };
 }
