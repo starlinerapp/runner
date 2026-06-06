41e145376719f4d60a1b1496583e4f76c01a433e
Leon Liang <hello@leonliang.lu>
2026-06-06 18:19:07 +0200

    fix

--- nix/modules/firecracker.nix
+++ nix/modules/firecracker.nix
@@ -1,7 +1,5 @@
-{ config, ... }:
-
 {
   system.activationScripts.runnerInit.text = ''
-    ln -sfn ${config.system.build.toplevel}/init /init
+    ln -sfn /nix/var/nix/profiles/system/init /init
   '';
 }
